ae97155bc35897bd128bddd4bef5cdb3d5eec8fd
Douglas Lara <douglas.lara@gmail.com>
2020-03-03 20:21:34 +0100

    first commit

---
+++ src/main/java/com/ubs/gcrg/cto/dfi/DfiApplication.java
@@ -0,0 +1,13 @@
+package com.ubs.gcrg.cto.dfi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DfiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DfiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ubs/gcrg/cto/dfi/DfiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ubs.gcrg.cto.dfi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DfiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
